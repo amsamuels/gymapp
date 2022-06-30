e9d1e91ca659edf9227114f47b03324aac1f3957
amsamuels <72213896+amsamuels@users.noreply.github.com>
2022-06-30 13:50:17 +0100

    Final Changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,14 +13,14 @@ export const Navbar = () => {
 	const [burger, setBurger] = useState(false);
 	const handleClick = () => setBurger(!burger);
 	return (
-		<div className="fixed w-full h-[150px] md:h-[125px] z-10 sm:px-16  py-4 flex justify-center items-center shadow-none bg-black">
-			<div className="flex items-center w-full md:flex-row flex-col justify-between md:pl-10 ">
-				<div className="relative item-center m-4">
+		<div className="fixed w-full h-[125px] z-10 sm:px-16 px-6 py-4 flex justify-center items-center shadow-none bg-black">
+			<div className="flex items-center w-full md:flex-row flex-col justify-between pt-12">
+				<div className="relative items-center m-4">
 					<Link to="/" className="">
 						<img
-							src={images.gym}
+							src={images.Logo}
 							alt="Logo"
-							className=" w-20 h-18 md:w-22 md:h-22"
+							className=" w-20 h-18 md:w-full md:h-28"
 						/>
 					</Link>
 				</div>
@@ -36,7 +36,7 @@ export const Navbar = () => {
 					</Stack>
 				</div>
 				<div
-					className="md:hidden  items-center  mt-2 mb-4 text-zinc-400"
+					className="md:hidden  items-center mt-2 mb-4 text-zinc-400"
 					onClick={handleClick}
 				>
 					{!burger ? (
@@ -47,25 +47,30 @@ export const Navbar = () => {
 				</div>
 				<ul
 					className={
-						!burger ? "hidden" : " flex flex-col bg-zinc-900 w-full py-6 px-10 "
+						!burger ? "hidden" : "rounded-md bg-zinc-900 w-[310px] py-4 px-4"
 					}
 				>
 					<motion.div
 						whileInView={{ y: [100, 0] }}
 						transition={{ duration: 1.5, ease: "easeOut" }}
-						className="flex flex-col justify-center items-center py-10 text-gray-600 bg-blend-lighten font-bold m-4 "
+						className="flex flex-col justify-center items-center py-4 text-white bg-blend-lighten m-2"
 					>
+						<Link to="/" className="">
+							<img
+								src={images.Logo}
+								alt="Logo"
+								className="rounded-md m-2 justify-center w-[70px] h-[70px]"
+							/>
+						</Link>
 						<Link
 							to="/"
-							className="p-4
-			  rounded-lg hover:bg-gray-400 text-xl font-bold font-nav"
+							className="p-4 rounded-lg hover:bg-gray-400 text-xl  font-hero"
 						>
 							Home
 						</Link>
 						<Link
 							to="/exercise/:id"
-							className="p-4 
-			  rounded-lg hover:bg-gray-400 text-xl font-bold font-nav"
+							className="p-4 rounded-lg hover:bg-gray-400 text-xl font-hero"
 						>
 							ExerciseDetails
 						</Link>
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,4 +1,5 @@
 import gym from "../assets/gym.png";
+import Logo from "../assets/Logo.png";
 import heroimg from "../assets/heroimg.png";
 import quote from "../assets/quote.png";
 import backimg from "../assets/backimg.png";
@@ -6,4 +7,13 @@ import bar from "../assets/bar.png";
 import leftarrow from "../assets/left-arrow.png";
 import rightarrow from "../assets/right-arrow.png";
 
-export default { gym, heroimg, quote, backimg, bar, leftarrow, rightarrow };
+export default {
+	Logo,
+	gym,
+	heroimg,
+	quote,
+	backimg,
+	bar,
+	leftarrow,
+	rightarrow,
+};
